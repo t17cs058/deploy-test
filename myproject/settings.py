f5dbd38d1cdf078e24eb46f1726ec9547ffce434
kohei nishimuta <einosturmi142@gmail.com>
2019-12-04 15:32:31 +0900

    Remove static direcory setting

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#    )
 
